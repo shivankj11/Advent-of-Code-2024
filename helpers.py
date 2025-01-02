0cf263bd4428ad474163a0654523bb6b3865ac1a
Shivank Joshi <89424061+shivankj11@users.noreply.github.com>
2025-01-02 15:34:37 -0800

    Added graph algo helpers, d23

--- helpers.py
+++ helpers.py
@@ -69,3 +69,79 @@ def suppress_stdout():
 def find_seq(A, seq):
     """ Returns indices of seq in A"""
     return np.where(reduce(op.and_, ((np.concatenate([(A == s)[i:], np.zeros(i, dtype=np.uint8)],dtype=np.uint8)) for i,s in enumerate(seq))))[0]
+
+def kosaraju(adj_list : Dict[Any, List[Any]])-> List[List[Any]]:
+    """ Returns list of strongly connected components for a directed graph
+        
+        PARAMETERS:
+            adj_list : Dictionary mapping Nodes -> List of neighboring nodes
+        
+        RETURNS:
+            List of strongly connected components (Lists of nodes)
+    """
+
+    # DFS for in-order traversal
+    visited = set()
+    stack = deque(adj_list.keys())
+
+    L = []
+    while stack:
+        v = stack.pop()
+        if v not in visited:
+            visited.add(v)
+            L.append(v)
+            stack.extend(adj_list[v])
+    
+    # Get reverse order traversal
+    L = L[::-1]
+
+    # Find SCCS
+    def assign(v, group : List, assigned : Set):
+        if v in assigned:
+            return group, assigned
+
+        assigned.add(v)
+        group.append(v)
+        for u in adj_list[v]:
+            if u not in assigned:
+                group.append(u)
+                assigned.add(u)
+            group, assigned = assign(u, group, assigned)
+        
+        return group, assigned
+
+    sccs = []
+    assigned = set()
+    for v in L:
+        group, assigned = assign(v, [], assigned)
+        sccs.append(group)
+    
+    return [scc for scc in sccs if scc]
+
+def bors_kerbosch(R, P, X, G, C):
+        """ Returns maximal cliques of G """
+        if len(P) == 0 and len(X) == 0:
+            if len(R) > 2:
+                C.append(sorted(R))            
+            return
+
+        pivot = max(P.union(X))
+        for v in P.difference(G[pivot]):
+            bors_kerbosch(R.union(set([v])), P.intersection(G[v]), X.intersection(G[v]), G, C)
+            P.remove(v)
+            X.add(v)
+
+def max_clique(adj_list : Dict[Any, List[Any]])-> List[List[Any]]:
+    """ Returns largest clique of a graph
+
+        PARAMETERS:
+            adj_list : Non-empty dictionary mapping Nodes -> List of neighboring nodes
+        
+        RETURNS:
+           Clique of maximum size of clique in the graph
+    """
+    cliques = []
+    bors_kerbosch(set([]), set(adj_list.keys()), set([]), adj_list, cliques)
+    
+    cliques.sort(key=len)
+    return cliques[-1]
